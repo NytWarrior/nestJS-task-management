220af856ef6f0d9de100e1a444ec1ed596480ec3
Rajeev <rajeevkr8917@gmail.com>
2023-05-11 17:02:48 +0530

    task model and taskCreate added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+        // console.log('title', title);
+        // console.log('description', description);
+        return this.tasksService.createTask(title, description);
+    }
+    // createTask(@Body() body) {
+    //     console.log('body', body);
+    // }
 }
